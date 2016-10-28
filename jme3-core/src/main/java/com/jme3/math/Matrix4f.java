6e549defec6e66a1e418736965c86dbdbd772a5b
Wiemken <emily.wiemken@capgemini.com>
2016-10-28 13:56:11 +0200

    update

--- jme3-core/src/main/java/com/jme3/math/Matrix4f.java
+++ jme3-core/src/main/java/com/jme3/math/Matrix4f.java
@@ -62,7 +62,7 @@ public final class Matrix4f implements Savable, Cloneable, java.io.Serializable 
     public float m10, m11, m12, m13;
     public float m20, m21, m22, m23;
     public float m30, m31, m32, m33;
-    public static final Matrix4f ZERO = new Matrix4f(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    public static final Matrix4f ZERO = new Matrix4f();
     public static final Matrix4f IDENTITY = new Matrix4f();
 
     /**
@@ -77,27 +77,27 @@ public final class Matrix4f implements Savable, Cloneable, java.io.Serializable 
     /**
      * constructs a matrix with the given values.
      */
-    public Matrix4f(float m00, float m01, float m02, float m03,
-            float m10, float m11, float m12, float m13,
-            float m20, float m21, float m22, float m23,
-            float m30, float m31, float m32, float m33) {
 
-        this.m00 = m00;
-        this.m01 = m01;
-        this.m02 = m02;
-        this.m03 = m03;
-        this.m10 = m10;
-        this.m11 = m11;
-        this.m12 = m12;
-        this.m13 = m13;
-        this.m20 = m20;
-        this.m21 = m21;
-        this.m22 = m22;
-        this.m23 = m23;
-        this.m30 = m30;
-        this.m31 = m31;
-        this.m32 = m32;
-        this.m33 = m33;
+    		
+    public Matrix4f(float[] row1, float[] row2, float[] row3, float[] row4) {
+    	this.m00 = row1[0];
+    	this.m01 = row1[1];
+        this.m02 = row1[2];
+        this.m03 = row1[3];
+        this.m10 = row2[0];
+        this.m11 = row2[1];
+        this.m12 = row2[2];
+        this.m13 = row2[3];
+        this.m20 = row3[0];
+        this.m21 = row3[1];
+        this.m22 = row3[2];
+        this.m23 = row3[3];
+        this.m30 = row4[0];
+        this.m31 = row4[1];
+        this.m32 = row4[2];
+        this.m33 = row4[3];
+    	
+   
     }
 
     /**
